7fa4d3adf192e3179c936970b6b3ec1e5958cb15
pinchai <pinchai.pc@gmail.com>
2026-05-15 10:07:03 +0700

    a

--- app.py
+++ app.py
@@ -1,23 +1,69 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 
 @app.get('/')
 def home():
-    return 'Hello Home Page'
+    return render_template('front/index.html')
+
+
+@app.get('/products')
+def products():
+    return render_template('front/products.html')
+
 
 @app.get('/product')
 def product():
-    return 'Hello From Product Page'
+    return render_template('front/product.html')
 
-@app.get('/about')
-def about():
-    return 'Hello From About Page'
 
-@app.get('/contact')
-def contact():
-    return 'Hello From Contact Page'
+@app.get('/cart')
+def cart():
+    return render_template('front/cart.html')
+
+
+@app.get('/account')
+def account():
+    return render_template('front/account.html')
+
+
+@app.get('/forgot-password')
+def forgot_password():
+    return render_template('front/forgot-password.html')
+
+
+@app.get('/login')
+def login():
+    return render_template('front/login.html')
+
+
+@app.get('/create-user')
+def create_user():
+    return render_template('front/create-user.html')
+
+
+@app.get('/checkout')
+def checkout():
+    return render_template('front/checkout.html')
+
+
+@app.get('/test_template')
+def test_template():
+    from product import products
+    for item in products:
+        item['qty'] = random.randint(1, 100)
+        item['discount_pct'] = random.randint(0, 100)
+        item['name'] = item['title'][:20]
+
+    # assert False, products[0]
+
+    return render_template(
+        'test_template.html',
+        products=products,
+    )
+
 
 if __name__ == '__main__':
     app.run()
